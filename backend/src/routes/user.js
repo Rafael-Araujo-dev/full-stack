1c832fd0c1bf86d598cf476a88e2aa72444a7a1b
Rafael-Araujo-dev <web.dev@orafaelribeiro.com>
2022-08-06 01:06:10 -0300

    feat: cria rota users

---
+++ backend/src/routes/user.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/users", (req, res) => {
+
+    const data = [
+        {
+            username: "rafael01",
+            email: "rafael01@mail.com",
+            name: "Rafael Araujo",
+        }
+    ];
+
+    res.json(req.data);
+    
+});
+
+module.exports = router;
