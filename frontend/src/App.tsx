a394241516a57b75bde1b0592f61449f44256206
Rafael-Araujo-dev <web.dev@orafaelribeiro.com>
2022-08-07 15:16:36 -0300

    feat: cria as rotas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+
+// Components
+import Navbar from "./components/layout/Navbar";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/SignIn" element={<SignIn />} />
+        <Route path="/SignUp" element={<SignUp />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
