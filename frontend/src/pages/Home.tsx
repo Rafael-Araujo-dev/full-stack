3514600572bacfe900b911f8cc26d70bd48779a6
Rafael-Araujo-dev <web.dev@orafaelribeiro.com>
2022-08-07 15:17:19 -0300

    feat: cria pagina home

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,15 @@
+// Components
+import Navbar from "../components/layout/Navbar";
+
+function Home() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Home Page</h1>
+      <br />
+      <p>Lorem ipsum dolor sit amet</p>
+    </div>
+  );
+}
+
+export default Home;
