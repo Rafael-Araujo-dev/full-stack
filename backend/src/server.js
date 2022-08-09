fa59f275924a4cf134bd1529e42c863c4d3adc08
Rafael-Araujo-dev <web.dev@orafaelribeiro.com>
2022-08-09 09:22:00 -0300

    feat: inicializa conexao com o mongodb

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,23 @@
-require("dotenv").config();
-const express = require("express");
+require("dotenv").config(); // Habilita o uso de variáveis de ambiente
+const express = require("express"); // Importa framework express
+const connectDB = require("./config/db"); // Importa função de conexão com o banco de dados
+
+// Importação dos middlewares
 const helmet  = require("./middlewares/helmet");
 
+// Define a porta onde nossa aplicação rodará
 const _PORT = process.env.PORT || 5000;
 
-const app = express();
+connectDB(); // Tenta se conectar com o banco de dados
 
-// Middlewares
+const app = express(); // Cria a apliação express
+
+// Inicialização dos middlewares e configurações
 app.use(helmet());
 app.disable("x-powered-by");
 
+// Definição das rotas para a nossa aplicação
 app.use("/api", require("./routes/user"));
 
+// Inicialização da aplicação
 app.listen(_PORT, () => console.log(`Server is running on port ${_PORT}`));
