6219ff67861a64d082e293b250cab3060a947768
Rafael-Araujo-dev <web.dev@orafaelribeiro.com>
2022-08-09 09:44:14 -0300

    feat: adiciona cors na aplicacao

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ const connectDB = require("./config/db"); // Importa função de conexão com o 
 
 // Importação dos middlewares
 const helmet  = require("./middlewares/helmet");
+const { cors, corsOptions } = require("./middlewares/cors");
 
 // Define a porta onde nossa aplicação rodará
 const _PORT = process.env.PORT || 5000;
@@ -15,6 +16,7 @@ const app = express(); // Cria a apliação express
 // Inicialização dos middlewares e configurações
 app.use(helmet());
 app.disable("x-powered-by");
+app.use(cors(corsOptions));
 
 // Definição das rotas para a nossa aplicação
 app.use("/api", require("./routes/user"));
