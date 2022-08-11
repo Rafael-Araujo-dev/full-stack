bde3eea50b2f2dd506c83616a79365a90280c620
Rafael-Araujo-dev <web.dev@orafaelribeiro.com>
2022-08-11 12:42:08 -0300

    fix: corrige configuracoes cors

--- backend/src/middlewares/cors.js
+++ backend/src/middlewares/cors.js
@@ -3,8 +3,9 @@ const cors = require("cors");
 // Configurações do cors
 const corsOptions = {
     // Define as origens permitidas para requisições
-    origin: [ 
-        process.env.NODE_ENV !== "production" && "http://localhost:3000"
+    origin: [
+        process.env.ALLOWED_ORIGIN,
+        process.env.NODE_ENV !== "production" && "http://localhost:3000",
     ],
     optionsSucessStatus: 200, 
 }
