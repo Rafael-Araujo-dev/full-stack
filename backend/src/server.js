0528010621a62250d4da2f3a1211fd5c3be8811a
Rafael-Araujo-dev <web.dev@orafaelribeiro.com>
2022-08-15 19:12:09 -0300

    fix: desativa middleware morgan em ambiente de desenvolvimento

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,7 +7,7 @@ const chalk = require("chalk");
 const helmet  = require("./middlewares/helmet");
 const cors = require("./middlewares/cors");
 const errorHandler = require("./middlewares/errorHandler");
-const morgan = require("./middlewares/morgan");
+process.env.NODE_ENV !== "development" && require("./middlewares/morgan").morgan;
 
 // Define a porta onde nossa aplicação rodará
 const _PORT = process.env.PORT || 5000;
@@ -19,7 +19,6 @@ const app = express();
 app.use(helmet);
 app.disable("x-powered-by");
 app.use(cors);
-app.use(morgan);
 
 app.use(express.json()); // Define os dados recebidos das requisições sendo do tipo JSON Object
 app.use(express.urlencoded({ extended: false}));
