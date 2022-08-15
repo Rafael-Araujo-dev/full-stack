41e6cba24952d43e28b32a1e4a11f9eb967ad565
Rafael-Araujo-dev <web.dev@orafaelribeiro.com>
2022-08-15 19:03:00 -0300

    feat: cria rota para funcao de login

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 // Importa as funções do controlador
-const { registerUser } = require("../controllers/userController");
+const { registerUser, loginUser } = require("../controllers/userController");
 
 // Cria as rotas
 router.post("/register", registerUser);
+router.post("/login", loginUser);
 
 // Exporta as rotas
 module.exports = router;
